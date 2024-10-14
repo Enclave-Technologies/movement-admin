fad1058f14bfdcebbc7b2cd3523c8257c05d036c
Avishek Majumder <avishekalex15@gmail.com>
2024-10-14 13:23:30 +0800

    tailwind fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,14 @@
+import "../globals.css";
+
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <main>
-      <h1>Auth pages</h1>
-      <div>{children}</div>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Auth pages</h1>
+            <div>{children}</div>
+        </main>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,41 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    colors: {
-      primary: "#006847",
-      white: "#FFFFFF",
-      black: "#000000",
-      gray: {
-        100: "#F7F7F7",
-        200: "#E5E5E5",
-        300: "#D4D4D4",
-        400: "#A3A3A3",
-        500: "#737373",
-        600: "#525252",
-        700: "#404040",
-        800: "#262626",
-        900: "#171717",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            colors: {
+                primary: "#006847",
+                white: "#FFFFFF",
+                black: "#000000",
+                gray: {
+                    100: "#F7F7F7",
+                    200: "#E5E5E5",
+                    300: "#D4D4D4",
+                    400: "#A3A3A3",
+                    500: "#737373",
+                    600: "#525252",
+                    700: "#404040",
+                    800: "#262626",
+                    900: "#171717",
+                },
+                blue: {
+                    500: "#3b82f6",
+                    600: "#2563eb",
+                    700: "#1d4ed8",
+                },
+            },
+        },
     },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
