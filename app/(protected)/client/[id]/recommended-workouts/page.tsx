37b839b42da301c55eea57315d8a784e90481075
Avishek Majumder <avishekalex15@gmail.com>
2024-10-17 00:44:10 +0800

    recommended workout loading

--- app/(protected)/client/[id]/recommended-workouts/page.tsx
+++ app/(protected)/client/[id]/recommended-workouts/page.tsx
@@ -3,12 +3,16 @@ import React, { useState, useEffect } from "react";
 import Breadcrumb from "@/components/Breadcrumb";
 import { useUser } from "@/context/ClientContext";
 import { useRouter } from "next/navigation";
+import axios from "axios";
+import BreadcrumbLoading from "@/components/BreadcrumbLoading";
 
 const Page = ({ params }: { params: { id: string } }) => {
     const router = useRouter();
-    const { userData } = useUser();
+    const { userData, setUserData } = useUser();
     const page_title = ["Training Program"];
     const id = params.id;
+    const [pageLoading, setPageLoading] = useState(true);
+    const [pendingOperations, setPendingOperations] = useState(2);
 
     // State variables for dropdown options
     const [firstDropdownOptions, setFirstDropdownOptions] = useState([
@@ -24,41 +28,109 @@ const Page = ({ params }: { params: { id: string } }) => {
     ]);
 
     // State variable for table data
-    const [tableData, setTableData] = useState<PhaseRow[]>([]);
+    const [phaseList, setPhaseList] = useState<PhaseRow[]>([]);
 
     // Example useEffect to load data for dropdowns and table
     useEffect(() => {
-        // Simulate an API call or data fetching
-        // const fetchDropdownData = async () => {
-        //     // Replace this with actual data fetching logic
-        //     const firstDropdownData = await fetchFirstDropdownData();
-        //     const secondDropdownData = await fetchSecondDropdownData();
-        //     setFirstDropdownOptions(firstDropdownData);
-        //     setSecondDropdownOptions(secondDropdownData);
-        // };
-        // const fetchTableData = async () => {
-        //     // Replace this with actual data fetching logic
-        //     const data = await fetchTableDataFromDB();
-        //     setTableData(data);
-        // };
-        // fetchDropdownData();
-        // fetchTableData();
-    }, []); // Empty dependency array means this runs once on mount
+        let isMounted = true; // To prevent state updates on unmounted components
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `http://127.0.0.1:8000/mvmt/v1/trainer/client?client_id=${params.id}`,
+                    {
+                        withCredentials: true, // Include cookies in the request
+                    }
+                );
+                if (isMounted) {
+                    setUserData(response.data); // Assuming setUserData updates the user data
+                }
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+            } finally {
+                if (isMounted) {
+                    console.log("USER DATA LOADING", pendingOperations);
+                    setPendingOperations((prev) => prev - 1); // Decrement pending operations
+                }
+            }
+        };
+
+        const performOtherAsyncOperations = async () => {
+            // Example of another async operation
+            try {
+                // Perform other async operations here
+                await new Promise((resolve) => setTimeout(resolve, 1000));
+            } catch (error) {
+                console.error("Error in other async operations:", error);
+            } finally {
+                if (isMounted) {
+                    console.log("OTHER ASYNC OPERATIONS", pendingOperations);
+                    setPendingOperations((prev) => prev - 1); // Decrement pending operations
+                }
+            }
+        };
+
+        setPageLoading(true); // Set loading to true before fetching data
+        setPendingOperations(2); // Set the number of pending operations
+
+        if (!userData) {
+            fetchData();
+        } else {
+            console.log("USERDATA FOUND!", pendingOperations);
+            setPendingOperations((prev) => prev - 1); // Decrement pending operations if userData is already available
+        }
+        performOtherAsyncOperations();
+        return () => {
+            isMounted = false; // Cleanup function to prevent state updates on unmounted components
+        };
+    }, [params.id, setUserData, userData]); // Empty dependency array means this runs once on mount
+
+    useEffect(() => {
+        if (pendingOperations === 0) {
+            setPageLoading(false); // Set loading to false when all operations are completed
+        }
+    }, [pendingOperations]);
 
     const handleAddPhase = () => {
         router.push(`/client/${id}/recommended-workouts/new-phase`);
     };
 
-    return (
+    return pageLoading ? (
+        <div>
+            <BreadcrumbLoading />
+            <div className="flex items-center space-x-4">
+                <div className="border rounded p-2 w-1/2 animate-pulse bg-gray-300">
+                    <div className="h-4 bg-gray-400 rounded"></div>
+                </div>
+                <span className="text-gray-500">&gt;</span>
+                <div className="border rounded p-2 w-1/2 animate-pulse bg-gray-300">
+                    <div className="h-4 bg-gray-400 rounded"></div>
+                </div>
+            </div>
+
+            <div className="mt-4">
+                <div className="w-full h-64 border-collapse border border-gray-300 animate-pulse bg-gray-200">
+                    <div className="h-full bg-gray-300"></div>
+                </div>
+            </div>
+
+            <button
+                onClick={handleAddPhase}
+                className="w-full mt-4 p-2 border-2 rounded animate-pulse bg-gray-300"
+                disabled
+            >
+                + Add Phase
+            </button>
+        </div>
+    ) : (
         <div>
             <Breadcrumb
                 homeImage={userData?.imageUrl}
                 homeTitle={userData?.name}
                 customTexts={page_title}
             />
-            Recommended
+
             <div className="flex items-center space-x-4">
-                <select className="border rounded p-2">
+                <select className="border rounded p-2 w-1/2">
                     {firstDropdownOptions.map((option) => (
                         <option key={option.value} value={option.value}>
                             {option.label}
@@ -66,7 +138,7 @@ const Page = ({ params }: { params: { id: string } }) => {
                     ))}
                 </select>
                 <span className="text-gray-500">&gt;</span>
-                <select className="border rounded p-2">
+                <select className="border rounded p-2 w-1/2">
                     {secondDropdownOptions.map((option) => (
                         <option key={option.value} value={option.value}>
                             {option.label}
@@ -75,52 +147,74 @@ const Page = ({ params }: { params: { id: string } }) => {
                 </select>
             </div>
             {/* Table Component */}
-            <table className="w-full mt-4 border-collapse border border-gray-300">
-                <thead>
-                    <tr>
-                        <th className="border border-gray-300 p-2">Order</th>
-                        <th className="border border-gray-300 p-2">Motion</th>
-                        <th className="border border-gray-300 p-2">
-                            Specific Description
-                        </th>
-                        <th className="border border-gray-300 p-2">Reps Min</th>
-                        <th className="border border-gray-300 p-2">Reps Max</th>
-                        <th className="border border-gray-300 p-2">Sets Min</th>
-                        <th className="border border-gray-300 p-2">Sets Max</th>
-                        <th className="border border-gray-300 p-2">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableData.map((row, index) => (
-                        <tr key={index}>
-                            <td className="border border-gray-300 p-2">
-                                {row.order}
-                            </td>
-                            <td className="border border-gray-300 p-2">
-                                {row.motion}
-                            </td>
-                            <td className="border border-gray-300 p-2">
-                                {row.specificDescription}
-                            </td>
-                            <td className="border border-gray-300 p-2">
-                                {row.repsMin}
-                            </td>
-                            <td className="border border-gray-300 p-2">
-                                {row.repsMax}
-                            </td>
-                            <td className="border border-gray-300 p-2">
-                                {row.setsMin}
-                            </td>
-                            <td className="border border-gray-300 p-2">
-                                {row.setsMax}
-                            </td>
-                            <td className="border border-gray-300 p-2">
-                                {/* {row.column3} */}
-                            </td>
+            {phaseList.length === 0 ? (
+                <p className="text-gray-600">
+                    No Exercises added to this session yet.
+                    <br />
+                    Click on + Add Session to add a new session.
+                </p>
+            ) : (
+                <table className="w-full mt-4 border-collapse border border-gray-300">
+                    <thead>
+                        <tr>
+                            <th className="border border-gray-300 p-2">
+                                Order
+                            </th>
+                            <th className="border border-gray-300 p-2">
+                                Motion
+                            </th>
+                            <th className="border border-gray-300 p-2">
+                                Specific Description
+                            </th>
+                            <th className="border border-gray-300 p-2">
+                                Reps Min
+                            </th>
+                            <th className="border border-gray-300 p-2">
+                                Reps Max
+                            </th>
+                            <th className="border border-gray-300 p-2">
+                                Sets Min
+                            </th>
+                            <th className="border border-gray-300 p-2">
+                                Sets Max
+                            </th>
+                            <th className="border border-gray-300 p-2">
+                                Action
+                            </th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {phaseList.map((row, index) => (
+                            <tr key={index}>
+                                <td className="border border-gray-300 p-2">
+                                    {row.order}
+                                </td>
+                                <td className="border border-gray-300 p-2">
+                                    {row.motion}
+                                </td>
+                                <td className="border border-gray-300 p-2">
+                                    {row.specificDescription}
+                                </td>
+                                <td className="border border-gray-300 p-2">
+                                    {row.repsMin}
+                                </td>
+                                <td className="border border-gray-300 p-2">
+                                    {row.repsMax}
+                                </td>
+                                <td className="border border-gray-300 p-2">
+                                    {row.setsMin}
+                                </td>
+                                <td className="border border-gray-300 p-2">
+                                    {row.setsMax}
+                                </td>
+                                <td className="border border-gray-300 p-2">
+                                    {/* {row.column3} */}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
             {/* Button Component */}
             <button
                 onClick={handleAddPhase}
