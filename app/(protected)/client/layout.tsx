be1cb30d8c2aacd4dbfd4bfc49b8c41188234151
Avishek Majumder <avishekalex15@gmail.com>
2024-10-25 01:58:57 +0800

    Back button

--- app/(protected)/client/layout.tsx
+++ app/(protected)/client/layout.tsx
@@ -1,16 +1,35 @@
 // app/(protected)/client/[id]/layout.tsx
 "use client";
 import { UserProvider } from "@/context/ClientContext";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
+import { IoIosArrowBack } from "react-icons/io";
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
     const params = useParams();
+    const router = useRouter();
     // Ensure params.id is defined
     if (!params.id) {
         return <div>Error: {JSON.stringify(params)}</div>;
     }
 
-    return <UserProvider params={params}>{children}</UserProvider>;
+    const handleBack = () => {
+        router.back();
+    };
+
+    return (
+        <UserProvider params={params}>
+            <div className="flex">
+                <button onClick={handleBack} className="w-16 h-16">
+                    <IoIosArrowBack
+                        className="text-gold-500 text-4xl font-bold
+                    hover:text-green-500"
+                    />
+                </button>
+
+                <div className="flex-grow p-2">{children}</div>
+            </div>
+        </UserProvider>
+    );
 };
 
 export default ClientLayout;
