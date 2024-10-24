e58801b50b93c822ff7076749bff30bbd28b4117
Avishek Majumder <avishekalex15@gmail.com>
2024-10-24 23:28:53 +0800

    primary green updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
             colors: {
-                primary: "#006847",
+                primary: "#006747",
                 white: "#FFFFFF",
                 black: "#000000",
                 gray: {
