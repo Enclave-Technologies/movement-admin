2a23e63ccbbbd86c763f156a16af1fc2e8e9b558
Avishek Majumder <avishekalex15@gmail.com>
2024-10-25 00:23:17 +0800

    client context refactor

--- app/(protected)/client/layout.tsx
+++ app/(protected)/client/layout.tsx
@@ -1,9 +1,16 @@
-// app/(protected)/client/layout.js
-
+// app/(protected)/client/[id]/layout.tsx
+"use client";
 import { UserProvider } from "@/context/ClientContext";
+import { useParams } from "next/navigation";
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
-    return <UserProvider>{children}</UserProvider>;
+    const params = useParams();
+    // Ensure params.id is defined
+    if (!params.id) {
+        return <div>Error: {JSON.stringify(params)}</div>;
+    }
+
+    return <UserProvider params={params}>{children}</UserProvider>;
 };
 
 export default ClientLayout;
