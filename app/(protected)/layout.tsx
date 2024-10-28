798d0c80d0f2c49497062adcc7aae3a63226db6a
Avishek Majumder <avishekalex15@gmail.com>
2024-10-28 13:51:38 +0800

    sync master

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,18 +2,20 @@ import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <main>
-      <div className="flex flex-col h-screen">
-        <div className="flex flex-1 flex-row">
-          <Sidebar />
-          <main className="flex-1 bg-gray-100">{children}</main>
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div className="flex flex-col h-screen">
+                <div className="flex flex-1 flex-row">
+                    <TrainerProvider>
+                        <Sidebar />
+                    </TrainerProvider>
+                    <main className="flex-1 bg-gray-100 px-4">{children}</main>
+                </div>
+            </div>
+        </main>
+    );
 }
