8f6c403a4c1983c7f82ee50b6a1e31c77c568f35
Avishek Majumder <avishekalex15@gmail.com>
2024-11-21 04:15:49 +0800

    removed unnecessary files

--- utils/mapPhaseToDropdown.tsx
+++
@@ -1,7 +0,0 @@
-export const mapPhaseToDropdown = (phases: Phase[]): PhaseDropdownOption[] => {
-    return phases.map((phase: Phase) => ({
-        value: phase.id,
-        label: phase.phaseName,
-        isActive: phase.isActive,
-    }));
-};
