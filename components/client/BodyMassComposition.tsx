c4f657046c57c3c4cb4ddf6d0aff7ce6108e2944
Avishek Majumder <avishekalex15@gmail.com>
2024-12-15 23:45:56 +0700

    bmc add to db

--- components/client/BodyMassComposition.tsx
+++ components/client/BodyMassComposition.tsx
@@ -8,75 +8,87 @@ import EditableTable from "@/components/pure-components/EditableTable";
 import { ID } from "appwrite";
 
 const BodyMassComposition = ({ client_id }) => {
-  const { userData } = useUser(); // Access the user data from Context
-  const page_title = ["BMI Records"];
-  const [bmiRecords, setBmiRecords] = useState([]);
+    const { userData } = useUser(); // Access the user data from Context
+    const page_title = ["BMI Records"];
+    const [bmiRecords, setBmiRecords] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get(
-          `${API_BASE_URL}/mvmt/v1/client/bmc?client_id=${client_id}`,
-          { withCredentials: true }
-        );
-        setBmiRecords(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(
+                    `${API_BASE_URL}/mvmt/v1/client/bmc?client_id=${client_id}`,
+                    { withCredentials: true }
+                );
+                setBmiRecords(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    function addNewEmptyRecord() {
+        console.log(bmiRecords);
+        setBmiRecords([
+            {
+                $id: ID.unique(),
+                // users: params.id,
+                DATE: new Date().toISOString().slice(0, 10),
+                HEIGHT: 0,
+                WEIGHT: 0,
+                CHIN: 0,
+                CHEEK: 0,
+                PEC: 0,
+                BICEPS: 0,
+                TRICEPS: 0,
+                SUBSCAP: 0,
+                MIDAX: 0,
+                SUPRA: 0,
+                "UPPER-THIGH": 0,
+                UBMIL: 0,
+                KNEE: 0,
+                CALF: 0,
+                QUAD: 0,
+            },
+            ...bmiRecords,
+        ]);
+        // bmiRecords.push({});
     }
 
-    fetchData();
-  }, []);
+    async function addBmcToDB(bmcData) {
+        console.log(bmcData);
 
-  function addNewEmptyRecord() {
-    console.log(bmiRecords);
-    setBmiRecords([
-      {
-        $id: ID.unique(),
-        // users: params.id,
-        DATE: new Date().toISOString().slice(0, 10),
-        HEIGHT: 0,
-        WEIGHT: 0,
-        CHIN: 0,
-        CHEEK: 0,
-        PEC: 0,
-        BICEPS: 0,
-        TRICEPS: 0,
-        SUBSCAP: 0,
-        MIDAX: 0,
-        SUPRA: 0,
-        "UPPER-THIGH": 0,
-        UBMIL: 0,
-        KNEE: 0,
-        CALF: 0,
-        QUAD: 0,
-      },
-      ...bmiRecords,
-    ]);
-    // bmiRecords.push({});
-  }
+        await axios.post(
+            `${API_BASE_URL}/mvmt/v1/client/bmc/${client_id}`,
+            { bmiRecord: bmcData },
+            { withCredentials: true }
+        );
+    }
 
-  return (
-    <div className="w-full">
-      <div className="w-full flex flex-row items-center justify-between mb-4">
-        <h1 className="text-xl font-bold text-black ml-2 leading-tight">
-          Body Mass Composition
-        </h1>
-        <button
-          onClick={() => {
-            addNewEmptyRecord();
-          }}
-          className="bg-primary text-white py-2 px-4 rounded-md"
-        >
-          + Add New Record
-        </button>
-      </div>
-      <EditableTable
-        data={bmiRecords}
-        emptyText="No BMI Records found"
-        headerColumns={BMC_COLUMNS}
-      />
-      {/* <div className="ml-12">
+    return (
+        <div className="w-full">
+            <div className="w-full flex flex-row items-center justify-between mb-4">
+                <h1 className="text-xl font-bold text-black ml-2 leading-tight">
+                    Body Mass Composition
+                </h1>
+                <button
+                    onClick={() => {
+                        addNewEmptyRecord();
+                    }}
+                    className="bg-primary text-white py-2 px-4 rounded-md"
+                >
+                    + Add New Record
+                </button>
+            </div>
+            <EditableTable
+                setData={setBmiRecords}
+                data={bmiRecords}
+                emptyText="No BMI Records found"
+                headerColumns={BMC_COLUMNS}
+                handleSaveBmc={addBmcToDB}
+            />
+            {/* <div className="ml-12">
                 <Breadcrumb
                     homeImage={userData?.imageUrl}
                     homeTitle={userData?.name}
@@ -179,8 +191,8 @@ const BodyMassComposition = ({ client_id }) => {
                     </button>
                 </div>
             </div> */}
-    </div>
-  );
+        </div>
+    );
 };
 
 export default BodyMassComposition;
