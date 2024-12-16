d6f29a5beb25adc2da3c5360e04d39b954c05b1b
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2024-12-16 11:23:33 +0800

    design: match Spinner to app-theme

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Spinner = () => {
-    return (
-        <div className="fixed inset-0 flex items-center justify-center h-full z-50 bg-gray-800/10">
-            <div className="relative">
-                <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-purple-500"></div>
-                <div className="absolute top-0 left-0 animate-ping rounded-full h-16 w-16 border-t-4 border-b-4 border-pink-500"></div>
-                <div className="absolute top-0 left-0 animate-pulse rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="fixed inset-0 flex items-center justify-center h-full z-50 bg-gray-800/10">
+      <div className="relative">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-green-500"></div>
+        <div className="absolute top-0 left-0 animate-ping rounded-full h-12 w-12 border-t-2 border-b-2 border-green-500"></div>
+        <div className="absolute top-0 left-0 animate-pulse rounded-full h-12 w-12 border-t-2 border-b-2 border-green-500"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Spinner;
