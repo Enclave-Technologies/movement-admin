0600258c149d784782cf5cee607b22c9df24f665
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2024-12-16 11:47:17 +0800

    bug: client/tab doesnt cover parent

--- components/client/Tabs.tsx
+++ components/client/Tabs.tsx
@@ -6,40 +6,40 @@ import { TbTargetArrow } from "react-icons/tb";
 import { Tab } from "./Tab";
 
 export const Tabs = ({ selectedTab, setSelectedTab }) => {
-    return (
-        <div className="flex flex-row items-center justify-between bg-white rounded-lg w-full border-b-[1px] border-gray-200">
-            <Tab
-                isSelected={selectedTab === "workout-history"}
-                label={"Workout History"}
-                onClick={() => {
-                    setSelectedTab("workout-history");
-                }}
-                icon={<MdChangeHistory size={20} />}
-            />
-            <Tab
-                isSelected={selectedTab === "workout-plan"}
-                label={"Workout Plan"}
-                onClick={() => {
-                    setSelectedTab("workout-plan");
-                }}
-                icon={<IoDocumentTextOutline size={20} />}
-            />
-            <Tab
-                isSelected={selectedTab === "goals"}
-                label={"Goals"}
-                onClick={() => {
-                    setSelectedTab("goals");
-                }}
-                icon={<TbTargetArrow size={20} />}
-            />
-            <Tab
-                isSelected={selectedTab === "body-mass-composition"}
-                label={"Body Mass Composition"}
-                onClick={() => {
-                    setSelectedTab("body-mass-composition");
-                }}
-                icon={<IoIosBody size={20} />}
-            />
-        </div>
-    );
+  return (
+    <div className="flex flex-row items-stretch justify-between bg-white rounded-lg w-full border-b-[1px] border-gray-200">
+      <Tab
+        isSelected={selectedTab === "workout-history"}
+        label={"Workout History"}
+        onClick={() => {
+          setSelectedTab("workout-history");
+        }}
+        icon={<MdChangeHistory size={20} />}
+      />
+      <Tab
+        isSelected={selectedTab === "workout-plan"}
+        label={"Workout Plan"}
+        onClick={() => {
+          setSelectedTab("workout-plan");
+        }}
+        icon={<IoDocumentTextOutline size={20} />}
+      />
+      <Tab
+        isSelected={selectedTab === "goals"}
+        label={"Goals"}
+        onClick={() => {
+          setSelectedTab("goals");
+        }}
+        icon={<TbTargetArrow size={20} />}
+      />
+      <Tab
+        isSelected={selectedTab === "body-mass-composition"}
+        label={"Body Mass Composition"}
+        onClick={() => {
+          setSelectedTab("body-mass-composition");
+        }}
+        icon={<IoIosBody size={20} />}
+      />
+    </div>
+  );
 };
