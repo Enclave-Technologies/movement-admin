a27a91b4f96182db6a3b317efcc8759dbc82ef77
Avishek Majumder <avishekalex15@gmail.com>
2025-02-21 16:16:36 +0800

    Update padding in ProfilePage layout for improved responsiveness

--- app/(protected)/(sidebarred)/trainer/[id]/page.tsx
+++ app/(protected)/(sidebarred)/trainer/[id]/page.tsx
@@ -26,7 +26,7 @@ export default async function ProfilePage({
     );
 
     return (
-        <div className="container mx-auto py-10">
+        <div className="container mx-auto px-6 py-10">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 <Card className="col-span-2">
                     <CardHeader>
