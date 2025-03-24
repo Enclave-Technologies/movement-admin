fd561a4c68aa78cdc6dcb854a53e95d6df6ca895
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2025-03-24 11:05:22 +0800

    bugfix: Features 1 & 5 (#69)
    * bugfix: Features 1 & 5
    
    * Update TableBody.tsx
    
    ---------
    
    Co-authored-by: Avishek Majumder <avishekalex15@gmail.com>

--- components/InfiniteScrollTable/TableBody.tsx
+++ components/InfiniteScrollTable/TableBody.tsx
@@ -2,39 +2,46 @@ import { MemoizedCell } from "./MemoizedTableComponents";
 import { Row } from "@tanstack/react-table";
 
 const TableBody = ({ rowVirtualizer, rows }) => {
-    return (
-        <tbody
-            style={{
-                display: "grid",
-                height: `${rowVirtualizer.getTotalSize() + 20}px`, //tells scrollbar how big the table is
-                position: "relative", //needed for absolute positioning of rows
-            }}
-        >
-            {rowVirtualizer.getVirtualItems().map((virtualRow) => {
-                const row = rows[virtualRow.index] as Row<any>;
-                return (
-                    <tr
-                        data-index={virtualRow.index}
-                        ref={(node) => rowVirtualizer.measureElement(node)}
-                        key={row.id}
-                        className={`${
-                            virtualRow.index % 2 ? "bg-white" : "bg-gray-100"
-                        } h-12 items-center cursor-default hover:bg-gray-200`}
-                        style={{
-                            display: "flex",
-                            position: "absolute",
-                            transform: `translateY(${virtualRow.start}px)`, //this should always be a `style` as it changes on scroll
-                            width: "100%",
-                        }}
-                    >
-                        {row.getVisibleCells().map((cell) => (
-                            <MemoizedCell key={cell.id} cell={cell} />
-                        ))}
-                    </tr>
-                );
-            })}
-        </tbody>
-    );
+  
+  return (
+    <tbody
+      style={{
+        display: "grid",
+        height: `${rowVirtualizer.getTotalSize() + 20}px`, //tells scrollbar how big the table is
+        position: "relative", //needed for absolute positioning of rows
+      }}
+    >
+      {rowVirtualizer.getVirtualItems().length === 0 ? (
+        <tr className={`bg-white h-12 items-center cursor-default`}>
+          <td colSpan={rows[0]?.getVisibleCells().length || 1} className="text-center">No Entries Found</td>
+        </tr>
+      ) : (
+        rowVirtualizer.getVirtualItems().map((virtualRow) => {
+          const row = rows[virtualRow.index] as Row<any>;
+          return (
+            <tr
+              data-index={virtualRow.index}
+              ref={(node) => rowVirtualizer.measureElement(node)}
+              key={row.id}
+              className={`${
+                virtualRow.index % 2 ? "bg-white" : "bg-gray-100"
+              } h-12 items-center cursor-default hover:bg-gray-200`}
+              style={{
+                display: "flex",
+                position: "absolute",
+                transform: `translateY(${virtualRow.start}px)`, //this should always be a `style` as it changes on scroll
+                width: "100%",
+              }}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <MemoizedCell key={cell.id} cell={cell} />
+              ))}
+            </tr>
+          );
+        })
+      )}
+    </tbody>
+  );
 };
 
 export default TableBody;
