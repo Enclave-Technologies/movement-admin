428b9ac583fdbfadf038c053abf06068ab687330
Avishek Majumder <avishekalex15@gmail.com>
2025-04-10 15:52:16 +0800

    refactor: remove unused dashboard components and pages

--- app/dashboard/tracked-exercises/page.tsx
+++
@@ -1,6 +0,0 @@
-const TrackedExercises = async () => {
-    const data = await fetch("https://jsonplaceholder.typicode.com/posts");
-    return <div>TrackedExercises</div>;
-};
-
-export default TrackedExercises;
