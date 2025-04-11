2b27a8c28db8e230a861cf51110d47b93fe9b8a2
Avishek Majumder <avishekalex15@gmail.com>
2025-04-12 02:06:13 +0800

    clean

--- app/(protected)/(sidebarred)/loading.js
+++
@@ -1,6 +0,0 @@
-export default function Loading() {
-  return (
-    // Use only tailwind CSS
-    <div className="min-h-screen"></div>
-  );
-}
--- components/drag-n-drop/Droppable.tsx
+++
@@ -1,5 +0,0 @@
-import { useDroppable } from "@dnd-kit/core";
-
-function Droppable({ setNodeRef, children }) {
-  return <div ref={setNodeRef}>{children}</div>;
-}
