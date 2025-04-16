3056c0f9691ece03e19dae8a8ecfcdaef1c8a650
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2025-04-17 01:39:42 +0800

    fix: behavior RxCross2 on ConfirmationDialog

--- components/ConfrmationDialog.tsx
+++ components/ConfrmationDialog.tsx
@@ -11,12 +11,14 @@ const ConfirmationDialog = ({
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-8 rounded-lg shadow-md">
-        <div
-          onClick={cancelAction}
-          className="w-full flex flex-row justify-end items-center pb-2"
-        >
-          <RxCross2 size={20} />
-        </div>
+        {cancelAction && (
+          <div
+            onClick={cancelAction}
+            className="w-full flex flex-row justify-end items-center pb-2"
+          >
+            <RxCross2 size={20} />
+          </div>
+        )}
         <div className="bg-white min-w-80 flex flex-col gap-8">
           <div className="flex flex-col items-start">
             <h3 className="text-xl font-semibold">{title}</h3>
